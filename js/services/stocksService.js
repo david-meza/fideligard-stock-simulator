c950ad4fce00f0a0bb2486a1071f8cd62fa9c3f1
Alice Fung <alice.o.fung@gmail.com>
2015-09-17 14:31:35 -0400

    stock panel calculation working

--- js/services/stocksService.js
+++ js/services/stocksService.js
@@ -1,13 +1,14 @@
 stocks.factory('stocksService', function() {
 
   var obj = {};
-  var _currentDate = "2014-12-02";
+  var _currentDate = "2014-11-02";
   var _index;
 
   var stockHistory = apple.query.results.quote;
 
   obj.setDate = function(selectedDate){
     _currentDate = selectedDate;
+    // getIndexInData();
   };
 
   obj.getDate = function(){
@@ -18,19 +19,23 @@ stocks.factory('stocksService', function() {
   var getIndexInData = function(){
     for (var i=0; i< stockHistory.length; i++){
       if (stockHistory[i].Date == _currentDate){
-        return i;
-      } else if (stockHistory[i].Date > _currentDate){
-        return i-1;
+        return _index = i;
+      } else if (stockHistory[i].Date < _currentDate){
+        return _index = i-1;
       }
     }
-    return 0; //if not found, edge case
+    return _index = 0; //if not found, edge case
   };
 
-  obj.priceChangeNDays = function(n){
-    var index = getIndexInData();
-    if (index-n < 0) return 0;
-    return stockHistory[index].Open - stockHistory[index-n].Open;
+  obj.priceChangeNDays = function(n, stock){
+    _currentDate = stock.Date;
+    getIndexInData();
+
+    if (_index-n < 0) return 0;
+    return stockHistory[_index].Open - stockHistory[_index-n].Open;
   };
+
+  getIndexInData();
 
   return obj;
 
