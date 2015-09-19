6efb0e4482679d5c0d0c1d7a449976fe6cd8325a
David <davidmeza@outlook.com>
2015-09-19 04:17:49 -0400

    add mock transactions data

--- js/controllers/transactionsCtrl.js
+++ js/controllers/transactionsCtrl.js
@@ -8,6 +8,12 @@ stocks.controller('transactionsCtrl',
 
     $scope.transactions = portfolioService.transactions;
 
+    $scope.selectSort = function(value) {
+      console.log("sorting by " + value)
+      $scope.reverse = ($scope.sortQuery === value) ? !$scope.reverse : false;
+      $scope.sortQuery = value;
+    };
+
 
   }]
 );
--- js/services/portfolioService.js
+++ js/services/portfolioService.js
@@ -2,6 +2,27 @@ stocks.factory('portfolioService', function() {
 
   var transactions = [];
 
+  var generateMockData = function() {
+    var symbols = ['GOOG', 'AAPL', 'FB', 'CVC', 'NFLX', 'AMZN', 'PFE', 'MSFT', 'C', 'F', 'NOK'];
+    for (var i = 0; i < 50; i++) {
+      var transaction = {
+        symbol: symbols[Math.floor(Math.random() * 11)],
+        date: randomDate(new Date("2012-01-01"), new Date()).toDateString(),
+        type: ["Buy", "Sell"][Math.floor(Math.random() * 2)],
+        quantity: Math.floor(Math.random() * 10000),
+        price: Math.random() * 500 + 50,
+        total: Math.random() * 500000,
+      }
+      transactions.push(transaction);
+    };
+  }
+
+  var randomDate = function (start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+  generateMockData();
+
 
   return {
     transactions: transactions
